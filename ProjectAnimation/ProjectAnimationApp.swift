9f5ee48153e5edc513366e08b350f4abde1ee080
Logan Snopek <LSnopek@lcs.on.ca>
2022-04-27 10:22:16 -0400

    Initial Commit

---
+++ ProjectAnimation/ProjectAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectAnimationApp.swift
+//  ProjectAnimation
+//
+//  Created by Logan Snopek on 2022-04-27.
+//
+
+import SwiftUI
+
+@main
+struct ProjectAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
